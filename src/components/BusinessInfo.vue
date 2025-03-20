e464d013464de5e359af8aefa60c00e395a56ccb
Hosseinee <nabil.hosseinee@gmail.com>
2025-03-20 12:44:27 +0100

    Fin contact

--- src/components/BusinessInfo.vue
+++ src/components/BusinessInfo.vue
@@ -11,13 +11,33 @@
         </div>
         <div class="business-contact">
             <h3>Contact</h3>
-            <div class="phone">
-                <p>Téléphone :</p>
-                <p>01 23 45 67 89</p>
+            <div class="phone-email">
+                <div class="phone">
+                    <p>Téléphone :</p>
+                    <p>01 23 45 67 89</p>
+                </div>
+                <div class="email">
+                    <p>Email :</p>
+                    <p>contact@gmail.com</p>
+                </div>
             </div>
-            <div class="email">
-                <p>Email :</p>
-                <p>contact@gmail.com</p>
+           
+        </div>
+        <div class="socials">
+            <h3>Réseaux sociaux</h3>
+            <div class="social-icons">
+                <div class="social-icon">
+                    <i class="fab fa-facebook"></i>
+                </div>
+                <div class="social-icon">
+                    <i class="fab fa-instagram"></i>
+                </div>
+                <div class="social-icon">
+                    <i class="fab fa-twitter"></i>
+                </div>
+                <div class="social-icon">
+                    <i class="fab fa-linkedin"></i>
+                </div>
             </div>
         </div>
     </div>
@@ -27,10 +47,72 @@
 <style scoped>
 .business-info {
     width: 40%;
-    height: 100%;
     background-color: #333;
     color: white;
     border-radius: 1rem;
-    padding: 1rem;
+    padding: 4rem;
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
 }
+
+.adress, .business-contact, .socials {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+
+.phone, .email {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+}
+
+.phone-email {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.social-icons {
+    display: flex;
+    justify-content: center;
+    gap: 3rem;
+}
+
+.social-icon {
+    font-size: 2rem;
+    cursor: pointer;
+    width: 4rem;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: #212121;
+    transition: all 0.3s ease;
+}
+
+.social-icon:hover {
+    transform: scale(1.1);
+    background-color: #f93232;
+}
+
+/* .social-icons {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 3rem;
+}
+
+.social-icons i {
+    font-size: 2rem;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+.social-icons i:hover {
+    transform: scale(1.1);
+    color: #f93232;
+} */
 </style>
